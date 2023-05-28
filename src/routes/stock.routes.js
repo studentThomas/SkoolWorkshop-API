3e86e5dd122824ab677d50a5ded3afb952e48ac2
studentThomas <twa.vermeulen@student.avans.nl>
2023-05-28 14:05:32 +0200

    Fixed bug
    Some routes where defined twice and controller name was from old commit.

--- src/routes/stock.routes.js
+++ src/routes/stock.routes.js
@@ -1,13 +1,15 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const stockController = require("../controllers/stock.controller");
+const stockController = require('../controllers/stock.controller');
+const cors = require('cors');
 
 router.use(express.json());
+router.use(cors());
 
 //UC301
-router.get("/stock/:productId", stockController.getStock);
+router.get('/stock/:productId', stockController.getStock);
 
 //UC302
-router.put("/stock/:productId", stockController.updateStock);
+router.put('/stock/:productId', stockController.updateStock);
 
 module.exports = router;
