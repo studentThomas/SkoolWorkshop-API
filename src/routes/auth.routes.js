1dfcb68cfe510c63d64fcacf5a3802c67053d016
Levi <gjj.kooy@student.avans.nl>
2023-05-30 10:50:09 +0200

    Added cors

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/auth.controller');
+const cors = require('cors');
+router.use(express.json());
+router.use(cors());
 
 //UC-101 Login
 router.post('/login', controller.login);
