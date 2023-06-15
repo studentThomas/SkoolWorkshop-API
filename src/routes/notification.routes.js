8dc69a36db0506019e67670585a89a4a226a074f
ibvandijk <117907237+ibvandijk@users.noreply.github.com>
2023-06-15 10:15:31 +0200

    added notifications

---
+++ src/routes/notification.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+const notificationController = require('../controllers/notification.controller');
+
+router.use(express.json());
+router.use(cors());
+
+router.get('/notifications', notificationController.getNotifications);
+
+
+module.exports = router;
